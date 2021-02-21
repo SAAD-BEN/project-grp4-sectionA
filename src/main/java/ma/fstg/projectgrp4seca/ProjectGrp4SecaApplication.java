27b901de67e8633ed6b2b30caf088ad92354fc36
saadben <saad.bentaleb@edu.uca.ma>
2021-02-21 16:19:20 +0100

    Initial commit

---
+++ src/main/java/ma/fstg/projectgrp4seca/ProjectGrp4SecaApplication.java
@@ -0,0 +1,13 @@
+package ma.fstg.projectgrp4seca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGrp4SecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectGrp4SecaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/fstg/projectgrp4seca/ProjectGrp4SecaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fstg.projectgrp4seca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGrp4SecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
